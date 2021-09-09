60866b42b6dade58b8bb11fe417c2ebe248bb494
yokotatororo <88578044+yokotatororo@users.noreply.github.com>
2021-09-09 23:52:28 +0900

    Add files via upload

---
+++ js3_YokotaMasanori_34/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "",
+    authDomain: "dev21chat-e695f.firebaseapp.com",
+    databaseURL: "https://dev21chat-e695f-default-rtdb.firebaseio.com/",
+    projectId: "dev21chat-e695f",
+    storageBucket: "dev21chat-e695f.appspot.com",
+    messagingSenderId: "848957456746",
+    appId: "1:848957456746:web:250dd383836ec87b35e513"
+  };
+
+  firebase.initializeApp(firebaseConfig);
